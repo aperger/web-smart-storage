f327c912d8073dabd8d001f6004808a64300c38b
Perger Attila <perger.attila@pergersoft.hu>
2026-02-21 01:11:57 +0100

    Move H2 config into right project

--- infra/src/main/java/hu/ps/ss/infra/database/config/TestProfileMemoryDBConfig.java
+++ infra/src/main/java/hu/ps/ss/infra/database/config/TestProfileMemoryDBConfig.java
@@ -1,23 +1,26 @@
-package hu.ps.ss.config;
+package hu.ps.ss.infra.database.config;
 
+import hu.ps.ss.infra.database.repository.VatKeyRepository;
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
-import ps.ss.entity.DocumentHeader;
+import hu.ps.ss.data.entity.VatKeyEntity;
 
-import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
 @Profile("test")
-@Import(JpaRepostoryConfig.class)
+@EntityScan(basePackageClasses = VatKeyEntity.class)
+@EnableJpaRepositories(basePackageClasses = {VatKeyRepository.class})
 public class TestProfileMemoryDBConfig {
 
 	@Bean
@@ -33,12 +36,11 @@ public class TestProfileMemoryDBConfig {
 	public LocalContainerEntityManagerFactoryBean entityManagerFactory(final DataSource dataSource) {
 		final LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
 		em.setDataSource(dataSource);
-		em.setPackagesToScan(DocumentHeader.class.getPackage().getName());
+		em.setPackagesToScan(VatKeyEntity.class.getPackage().getName());
 		em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
 		em.setJpaProperties(additionalProperties());
 		return em;
 	}
-
 	
 	@Bean
 	PlatformTransactionManager transactionManager(final EntityManagerFactory entityManagerFactory) {
@@ -56,7 +58,7 @@ public class TestProfileMemoryDBConfig {
 		hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
 		hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
 		hibernateProperties.setProperty("hibernate.show_sql", "true");
-		hibernateProperties.setProperty("hibernate.enable_lazy_load_no_trans", "true");
+		hibernateProperties.setProperty("hibernate.enable_lazy_load_no_trans", "false");
 
 		return hibernateProperties;
 	}
