a8cf76c5b89a0554079cbe29014119ce5ee52a9b
Perger Attila <perger.attilla@pergersoft.hu>
2026-09-15 22:51:20 +0200

    feat(frontend): bootstrap ionic PWA shell with smart-storage-frontend

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'frontend/smart-storage-frontend',
+  webDir: 'www'
+};
+
+export default config;
